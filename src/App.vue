96e7d210ea20c30d88a25acc7a373f7b3e6aa868
wangkai <812435424@qq.com>
2017-09-25 09:09:04 +0800

    测试使用

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <wk-footer></wk-footer>
+  </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer/Footer'
+
+export default {
+  name: 'app',
+  comments:{
+    'wk-footer': Footer
+  }
+}
+</script>
+
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="footer">1112</div>
+</template>
+<script>
+    export default {
+        name: 'footer'
+    }
+</script>
